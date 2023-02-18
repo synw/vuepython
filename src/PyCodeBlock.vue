11ae30cdbb68036c939bab94f06286f12383583e
synw <synwx@protonmail.com>
2023-02-18 15:52:19 +0100

    Version 0.0.12: improvements in PyCodeBlock

--- src/PyCodeBlock.vue
+++ src/PyCodeBlock.vue
@@ -1,29 +1,42 @@
 <template>
   <div>
-    <code-editor :code="parsedCode" @keyup.ctrl.enter="runTheCode()" lang="python" @edit="updateCode($event)"
-      :hljs="hljs"></code-editor>
+    <div class="code-editor">
+      <code-editor :code="parsedCode" @keyup.ctrl.enter="runTheCode()" lang="python" @edit="updateCode($event)"
+        :hljs="hljs"></code-editor>
+    </div>
     <div v-if="controls == true">
-      <button class="mt-3 border btn" :class="canRun ? 'cursor-pointer' : 'cursor-wait'" @click="runTheCode()">
-        <app-icon name="play" class="inline-block text-xl" :class="!canRun ? 'txt-lighter' : 'txt-success'"></app-icon>
+      <button class="border code-exec-btn btn neuro" :class="(canRun && !isExecuting) ? 'cursor-pointer' : 'cursor-wait'"
+        @click="runTheCode()">
+        <template v-if="!canRun">
+          <app-icon name="play" class="inline-block text-xl txt-lighter"></app-icon>
+        </template>
+        <template v-else>
+          <template v-if="!isExecuting">
+            <app-icon name="play" class="inline-block text-xl txt-success"></app-icon>
+          </template>
+          <template v-else>
+            <app-icon name="python" class="inline-block text-xl txt-danger"></app-icon>
+          </template>
+        </template>
         &nbsp;Execute
       </button>
     </div>
-    <div class="flex flex-col pl-8">
-      <div class="mt-3" v-if="stdout == true && outputHtml">
+    <div class="flex flex-col code-output">
+      <div v-if="stdout == true && outputHtml">
         <pre v-for="row in log.stdOut" v-html="row"></pre>
       </div>
-      <div class="mt-3" v-if="stderr == true">
+      <div v-if="stderr == true">
         <pre v-for="row in log.stdErr" v-html="row"></pre>
       </div>
-      <div class="mt-3" v-if="exception == true && log.exception" v-html="log.exception"></div>
-      <div class="mt-3" v-if="output == true && outputHtml" v-html="outputHtml"></div>
+      <div v-if="exception == true && log.exception" v-html="log.exception"></div>
+      <div v-if="output == true && outputHtml" v-html="outputHtml"></div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import "highlight.js";
-import { computed, onBeforeUnmount, reactive, ref, watchEffect } from "vue";
+import { computed, onBeforeUnmount, reactive, ref, Ref, watchEffect } from "vue";
 import { usePython } from "usepython";
 import { useStore } from '@nanostores/vue';
 import { CodeEditor } from "vuecodit";
@@ -82,6 +95,7 @@ const log = reactive({
   stdErr: new Array<string>(),
   exception: ""
 });
+const isExecuting: Readonly<Ref<boolean>> = useStore(props.py.isExecuting);
 
 function updateCode(code: string) {
   parsedCode.value = code;
@@ -134,3 +148,13 @@ watchEffect(() => {
   outputHtml.value = "";
 })
 </script>
+
+<style lang="sass" scoped>
+.code-exec-btn
+  @apply mt-2
+.code-output
+  @apply pl-8 space-y-2
+.btn.neuro
+  @apply rounded-lg block-background shadow-[-2px_-2px_10px_rgba(255,255,255,1),3px_3px_10px_rgba(0,0,0,0.2)] active:shadow-[inset_-2px_-2px_5px_rgba(255,255,255,0.7),inset_3px_3px_5px_rgba(0,0,0,0.1)]
+  @apply dark:shadow-[-2px_-2px_10px_rgba(155,155,155,0.3),3px_3px_10px_rgba(10,10,10,1)]
+</style>
